f1e4f0265bb7c1e1b3d1ff522705edd19206fc53
Willy Mills <orwonthe@gmail.com>
2022-08-26 05:15:09 -0600

    Fix blueprint registration

--- www/testing/test_registration.py
+++ www/testing/test_registration.py
@@ -1,6 +1,6 @@
 # Copyright 2022 WillyMillsLLC
 from www.testing.fixtures_for_testing import pretend_application
-from www.tracking.blueprint_registration import blueprint_registeration
+from www.tracking.blueprint_registration import blueprint_registration
 
 
 def _pycharm_please_keep_these_imports():
@@ -8,7 +8,7 @@ def _pycharm_please_keep_these_imports():
 
 
 def test_registration(pretend_application):
-    blueprint_registeration(pretend_application)
+    blueprint_registration(pretend_application)
 
     expected_blueprints = [
         'group_bp',
@@ -18,8 +18,12 @@ def test_registration(pretend_application):
         'things_bp',
     ]
     for blueprint_name in expected_blueprints:
+        blueprint_prefix = blueprint_name.split("_")[0]  # "things_bp" --> "things"
         registration = pretend_application.blueprints.get(blueprint_name)
         assert registration is not None
         blueprint = registration.get("blueprint")
         assert blueprint is not None
         assert blueprint.name == blueprint_name
+        url_prefix = registration.get("url_prefix")
+        assert url_prefix is not None
+        assert url_prefix.endswith(blueprint_prefix)
--- www/tracking/blueprint_registration.py
+++ www/tracking/blueprint_registration.py
@@ -6,24 +6,25 @@ def my_url(url):
     return f'{base_url}{url}'
 
 
+GROUP_PREFIX = my_url('/group')
 HOME_PREFIX = my_url('/home')
 PEOPLE_PREFIX = my_url('/people')
 PLACES_PREFIX = my_url('/places')
 THINGS_PREFIX = my_url('/things')
 
 
-def blueprint_registeration(target_application):
+def blueprint_registration(target_application):
     from www.tracking.home.home_routes import home_bp
     target_application.register_blueprint(home_bp, url_prefix=HOME_PREFIX)
 
     from www.tracking.groups.group_routes import group_bp
-    target_application.register_blueprint(group_bp, url_prefix=HOME_PREFIX)
+    target_application.register_blueprint(group_bp, url_prefix=GROUP_PREFIX)
 
     from www.tracking.people.people_routes import people_bp
-    target_application.register_blueprint(people_bp, url_prefix=HOME_PREFIX)
+    target_application.register_blueprint(people_bp, url_prefix=PEOPLE_PREFIX)
 
     from www.tracking.places.place_routes import places_bp
-    target_application.register_blueprint(places_bp, url_prefix=HOME_PREFIX)
+    target_application.register_blueprint(places_bp, url_prefix=PLACES_PREFIX)
 
     from www.tracking.things.thing_routes import things_bp
-    target_application.register_blueprint(things_bp, url_prefix=HOME_PREFIX)
+    target_application.register_blueprint(things_bp, url_prefix=THINGS_PREFIX)
