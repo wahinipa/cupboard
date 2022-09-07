3b3782a5ff015ae17ddf33a344a0a67e1186133c
Willy Mills <orwonthe@gmail.com>
2022-09-07 09:33:09 -0600

    Make Thing a hierarchy

--- www/testing/test_things_model.py
+++ www/testing/test_things_model.py
@@ -1,5 +1,6 @@
 #  Copyright (c) 2022, Wahinipa LLC
-from testing.fixtures_for_testing import app, light_saber, LIGHT_SABER_THING_NAME, LIGHT_SABER_THING_DESCRIPTION, LIGHT_SABER_THING_DATE
+from testing.fixtures_for_testing import app, light_saber, LIGHT_SABER_THING_NAME, LIGHT_SABER_THING_DESCRIPTION, \
+    LIGHT_SABER_THING_DATE
 from tracking.things.thing_models import find_or_create_thing
 
 
@@ -11,6 +12,10 @@ def test_thing_creation(light_saber):
     assert light_saber.name == LIGHT_SABER_THING_NAME
     assert light_saber.description == LIGHT_SABER_THING_DESCRIPTION
     assert light_saber.date_created == LIGHT_SABER_THING_DATE
+    kind = light_saber.kind_of
+    assert kind is not None
+    assert kind.name == "Everything"
+    assert light_saber in kind.kinds
 
 
 def test_things_are_unique_by_name(light_saber):
--- www/tracking/things/thing_models.py
+++ www/tracking/things/thing_models.py
@@ -1,22 +1,28 @@
 # Copyright (c) 2022, Wahinipa LLC
 from datetime import datetime
 
+from sqlalchemy.orm import backref
+
 from tracking import database
 
 
 class Thing(database.Model):
     id = database.Column(database.Integer, primary_key=True)
+    kind_of_id = database.Column(database.Integer, database.ForeignKey('thing.id'), index=True)
     name = database.Column(database.String(255), nullable=False, unique=True)
     description = database.Column(database.Text(), nullable=False, server_default='')
     date_created = database.Column(database.DateTime(), default=datetime.now())
+    kinds = database.relationship('Thing', backref=backref('kind_of', remote_side='Thing.id'))
 
 
-def find_or_create_thing(name, description, date_created=None):
+def find_or_create_thing(name, description, kind_of=None, date_created=None):
     thing = find_thing_by_name(name)
     if thing is None:
+        if kind_of is None:
+            kind_of = find_or_create_everything()
         if date_created is None:
             date_created = datetime.now()
-        thing = Thing(name=name, description=description, date_created=date_created)
+        thing = Thing(name=name, description=description, kind_of_id=kind_of.id, date_created=date_created)
         database.session.add(thing)
         database.session.commit()
     return thing
@@ -24,3 +30,11 @@ def find_or_create_thing(name, description, date_created=None):
 
 def find_thing_by_name(name):
     return Thing.query.filter(Thing.name == name).first()
+
+def find_or_create_everything():
+    everything = find_thing_by_name("Everything")
+    if everything is None:
+        everything = Thing(name="Everything", description="All things")
+        database.session.add(everything)
+        database.session.commit()
+    return everything
