b19ab182ec71bc1272091ea68e06a7a2481a16eb
Willy Mills <orwonthe@gmail.com>
2022-10-19 09:06:05 -0600

    cosmetic file rename for consistency

--- www/tracking/commons/cupboard_navigation.py
+++ www/tracking/commons/cupboard_navigation.py
@@ -1,10 +1,10 @@
 #  Copyright (c) 2022, Wahinipa LLC
 from tracking.navigation.category_navigation import register_category_navigation
 from tracking.navigation.navigator import Navigator
-from tracking.navigation.people_navigator import register_people_navigation
-from tracking.navigation.place_navigator import register_place_navigation
-from tracking.navigation.root_navigator import register_root_navigation
-from tracking.navigation.thing_navigator import register_thing_navigation
+from tracking.navigation.people_navigation import register_people_navigation
+from tracking.navigation.place_navigation import register_place_navigation
+from tracking.navigation.root_navigation import register_root_navigation
+from tracking.navigation.thing_navigation import register_thing_navigation
 
 
 def create_cupboard_navigator():
