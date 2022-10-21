fd5eef9bd6cbb9efd0c2323aab524a74119e80c0
Willy Mills <orwonthe@gmail.com>
2022-10-20 20:08:53 -0600

    Simplify dual_navigator logic

--- www/tracking/navigation/dual_navigator.py
+++ www/tracking/navigation/dual_navigator.py
@@ -17,80 +17,57 @@ def root_url(root, place=None, thing=None, task='view'):
     return url_for(f'root_bp.root_{task}', root_id=root.id, place_id=place.id, thing_id=thing.id)
 
 
-def categories_url(root, place, thing, task='view'):
-    return url_for(f'categories_bp.categories_{task}', root_id=root.id, place_id=place.id, thing_id=thing.id)
-
-
-def category_url(category, place, thing, task='view'):
-    return url_for(f'category_bp.category_{task}', category_id=category.id, place_id=place.id, thing_id=thing.id)
-
-
-def category_url(category, place, thing, task='view'):
-    return url_for(f'category_bp.category_{task}', category_id=category.id, place_id=place.id, thing_id=thing.id)
-
-
-def choice_url(choice, place, thing, task='view') -> object:
-    return url_for(f'choice_bp.choice_{task}', choice_id=choice.id, place_id=place.id, thing_id=thing.id)
-
-
-def place_url(place, thing, task='view') -> object:
-    return url_for(f'place_bp.place_{task}', place_id=place.id, thing_id=thing.id)
-
-def thing_url(place, thing, task='view') -> object:
-    return url_for(f'thing_bp.thing_{task}', place_id=place.id, thing_id=thing.id)
-
-
 class DualNavigator:
     def __init__(self, root=None, place=None, thing=None):
         from tracking.navigation.cupboard_navigation import create_cupboard_navigator
         self.navigator = create_cupboard_navigator()
-        self.root_mark = navigational_mark(Root)
-        self.place_mark = navigational_mark(Place)
-        self.thing_mark = navigational_mark(Thing)
-        self.categories_mark = navigational_mark(Categories)
-        self.category_mark = navigational_mark(Category)
-        self.choice_mark = navigational_mark(Choice)
-
+        self.translator = {
+            navigational_mark(Categories): self.categories_url,
+            navigational_mark(Category): self.category_url,
+            navigational_mark(Choice): self.choice_url,
+            navigational_mark(Place): self.place_url,
+            navigational_mark(Root): self.root_url,
+            navigational_mark(Thing): self.thing_url,
+        }
         if root is None:
             if place:
                 root = place.root
             elif thing:
                 root = thing.root
-        if place is None and root is not None:
-            place = root.place
-        if thing is None and root is not None:
-            thing = root.thing
-        self.root = root
-        self.place = place
-        self.thing = thing
+        place = place or root.place
+        thing = thing or root.thing
+        self.root_id = root.id if root else None
+        self.place_id = place.id if place else None
+        self.thing_id = thing.id if thing else None
+
+    def default_url(self, target, task):
+        return self.navigator.url(target, task)
+
+    def categories_url(self, categories, task):
+        return url_for(f'categories_bp.categories_{task}', root_id=self.root_id, place_id=self.place_id,
+                       thing_id=self.thing_id)
+
+    def category_url(self, category, task):
+        return url_for(f'category_bp.category_{task}', category_id=category.id, place_id=self.place_id,
+                       thing_id=self.thing_id)
+
+    def choice_url(self, choice, task):
+        return url_for(f'choice_bp.choice_{task}', choice_id=choice.id, place_id=self.place_id, thing_id=self.thing_id)
+
+    def place_url(self, place, task):
+        if task == 'view':
+            return self.root_url(place.root, task, place_id=place.id)
+        url_for(f'place_bp.place_{task}', place_id=place.id, thing_id=self.thing_id)
+
+    def root_url(self, root, task, place_id=None, thing_id=None):
+        place_id = place_id or self.place_id or root.place.id
+        thing_id = thing_id or self.thing_id or root.thing.id
+        return url_for(f'root_bp.root_{task}', root_id=root.id, place_id=place_id, thing_id=thing_id)
+
+    def thing_url(self, thing, task):
+        if task == 'view':
+            return self.root_url(thing.root, task, thing_id=thing.id)
+        return url_for(f'thing_bp.thing_{task}', place_id=self.place_id, thing_id=thing.id)
 
     def url(self, target, task):
-        # TODO: simplify this mess. Dictionary? Mini classes?
-        if task == 'view':
-            if self.root is None:
-                if navigational_mark(target) == self.root_mark:
-                    return root_url(target, target.place, target.thing)
-            else:
-                if navigational_mark(target) == self.root_mark:
-                    return root_url(target, self.place, self.thing)
-                else:
-                    if navigational_mark(target) == self.place_mark:
-                        return root_url(self.root, target, self.thing)
-                    elif navigational_mark(target) == self.thing_mark:
-                        return root_url(self.root, self.place, target)
-                    elif navigational_mark(target) == self.categories_mark:
-                        return categories_url(self.root, self.place, self.thing)
-                    elif navigational_mark(target) == self.category_mark:
-                        return category_url(target, self.place, self.thing)
-        if navigational_mark(target) == self.root_mark:
-            return root_url(target, self.place, self.thing, task=task)
-        elif navigational_mark(target) == self.category_mark:
-            return category_url(target, self.place, self.thing, task=task)
-        elif navigational_mark(target) == self.choice_mark:
-            return choice_url(target, self.place, self.thing, task=task)
-        elif navigational_mark(target) == self.place_mark:
-            return place_url(target, self.thing, task=task)
-        elif navigational_mark(target) == self.thing_mark:
-            return thing_url(self.place, target, task=task)
-
-        return self.navigator.url(target, task)
+        return self.translator.get(navigational_mark(target), self.default_url)(target, task)
