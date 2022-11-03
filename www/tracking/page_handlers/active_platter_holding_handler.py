4e277f728ae270ce27fa2e5be61783c0cd6d6780
Willy Mills <orwonthe@gmail.com>
2022-11-03 15:47:01 -0600

    Extract PlatterHoldingHandler base class from ActivePlatterHoldingHandler

--- www/tracking/page_handlers/active_platter_holding_handler.py
+++ www/tracking/page_handlers/active_platter_holding_handler.py
@@ -1,26 +1,8 @@
 #  Copyright (c) 2022, Wahinipa LLC
-from tracking.navigation.platter import PlatterById
-from tracking.page_handlers.viewer_holding_handler import ViewerHoldingHandler
-from tracking.viewers.categories_model import Categories
+from tracking.page_handlers.platter_holding_handler import PlatterHoldingHandler
 
 
-class ActivePlatterHoldingHandler(ViewerHoldingHandler):
-
-    def __init__(self, viewer, **kwargs):
-        ViewerHoldingHandler.__init__(self, viewer)
-        self.platter = PlatterById(**kwargs)
-
-    @property
-    def navigator(self):
-        return self.platter
-
-    @property
-    def category_list_url(self):
-        return self.navigator.url(Categories, 'view')
-
-    @property
-    def current_quantity(self):
-        return self.platter.current_quantity
+class ActivePlatterHoldingHandler(PlatterHoldingHandler):
 
     @property
     def is_valid(self):
@@ -33,32 +15,9 @@ class ActivePlatterHoldingHandler(ViewerHoldingHandler):
     # This method allows derived classes to override @property objects_are_valid
     # and still call this parent method without confusing syntax.
     def base_says_objects_are_valid(self):
-        return self.platter.may_be_observed(self.viewer)
+        return self.platter_objects_are_valid
 
     @property
     def objects_are_valid(self):
         return self.base_says_objects_are_valid()
 
-    @property
-    def place(self):
-        return self.platter.place
-
-    @property
-    def place_url(self):
-        return self.navigator.url(self.root, 'view')
-
-    @property
-    def root(self):
-        return self.platter.root
-
-    @property
-    def specification(self):
-        return self.platter.specification
-
-    @property
-    def thing(self):
-        return self.platter.thing
-
-    @property
-    def thing_specification(self):
-        return self.platter.thing_specification
---
+++ www/tracking/page_handlers/platter_holding_handler.py
@@ -0,0 +1,51 @@
+#  Copyright (c) 2022, Wahinipa LLC
+from tracking.navigation.platter import PlatterById
+from tracking.page_handlers.viewer_holding_handler import ViewerHoldingHandler
+from tracking.viewers.categories_model import Categories
+
+
+class PlatterHoldingHandler(ViewerHoldingHandler):
+
+    def __init__(self, viewer, **kwargs):
+        ViewerHoldingHandler.__init__(self, viewer)
+        self.platter = PlatterById(**kwargs)
+
+    @property
+    def platter_objects_are_valid(self):
+        return self.platter.may_be_observed(self.viewer)
+
+    @property
+    def navigator(self):
+        return self.platter
+
+    @property
+    def category_list_url(self):
+        return self.navigator.url(Categories, 'view')
+
+    @property
+    def current_quantity(self):
+        return self.platter.current_quantity
+
+    @property
+    def place(self):
+        return self.platter.place
+
+    @property
+    def place_url(self):
+        return self.navigator.url(self.root, 'view')
+
+    @property
+    def root(self):
+        return self.platter.root
+
+    @property
+    def specification(self):
+        return self.platter.specification
+
+    @property
+    def thing(self):
+        return self.platter.thing
+
+    @property
+    def thing_specification(self):
+        return self.platter.thing_specification
